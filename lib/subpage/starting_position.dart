85977512e88af88ff4e800d9f482f7898ed3818b
Aadile TOUMJI <atoumji@norsys.fr>
2020-07-03 14:48:20 +0200

    feat(step6_startingposition) add info of starting position on page actual position

--- lib/subpage/starting_position.dart
+++ lib/subpage/starting_position.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:out_of_bound_ato/provider/location_provider.dart';
+import 'package:provider/provider.dart';
 
 class StartPosition extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print("Start Position Page OK");
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(5),
-          child: Text("page Start Position")
-      ),
-    );
+    return Consumer<LocationProvider>(builder: (context, _locationProvider, _) {
+      print("Start Position Page OK");
+      return Scaffold(
+        backgroundColor: Colors.blue,
+        body: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(5),
+            child: _locationProvider.startingPosition == null
+                ?  Text(
+                "Please, click a button as start on page actual position, thank ")
+                : Text(
+                "My Last Position ${_locationProvider?.startingPosition.latitude} and ${_locationProvider.startingPosition.longitude}")
+        ),
+      );
+    });
   }
 }
