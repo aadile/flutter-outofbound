23ecff63feaafea31c5a4358c841939cf035cec5
Aadile TOUMJI <atoumji@norsys.fr>
2020-07-09 11:22:49 +0200

    feat(step12) custom navbar : animatedAlign show color orange on menu button active

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:out_of_bound_ato/provider/location_provider.dart';
 import 'package:out_of_bound_ato/provider/settings_provider.dart';
 import 'package:out_of_bound_ato/subpage/actual_position.dart';
@@ -16,16 +17,14 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => LocationProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => SettingsProvider(),
-        )
-      ],
-      child: MaterialApp(title: "Welcome to World Flutter", home: HomePage()));
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (context) => LocationProvider(),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => SettingsProvider(),
+      )
+    ], child: MaterialApp(title: "Welcome to World Flutter", home: HomePage()));
   }
 }
 
@@ -37,7 +36,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int selectedButton = 0;
   final choiceButton = [StartPosition(), ActualPosition(), SettingsPage()];
-  final choiceColorButton = [Colors.blue, Colors.red, Colors.green];
+
+  Alignment _alignment = Alignment.bottomLeft;
 
   @override
   Widget build(BuildContext context) {
@@ -47,60 +47,96 @@ class _HomePageState extends State<HomePage> {
         bottomNavigationBar: Container(
             height: 40,
             alignment: Alignment.bottomCenter,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
+            child: Stack(children: <Widget>[
+              AnimatedAlign(
+                duration: Duration(milliseconds: 500),
+                alignment: _alignment,
+                child: Container(
+                  width: 135,
+                  //height: kBottomNavigationBarHeight,
+                  padding: EdgeInsets.all(1),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
                 Expanded(
-                  child: RaisedButton.icon(
+                  child: FlatButton.icon(
                       onPressed: () {
                         setState(() {
                           selectedButton = 0;
+                          _alignment = Alignment.bottomLeft;
                           print("selected Button Starting Location");
                         });
                       },
-                      icon: Icon(Icons.location_city),
+                      icon: Icon(Icons.location_city,
+                          color:
+                              selectedButton == 0 ? Colors.white : Colors.grey),
                       label: Flexible(
                           child: Text("Starting location",
-                              style: new TextStyle(fontSize: TEXT_SIZE),
+                              style: new TextStyle(
+                                  fontSize: TEXT_SIZE,
+                                  color: selectedButton == 0
+                                      ? Colors.white
+                                      : Colors.grey),
                               textAlign: TextAlign.center)),
-                      color: Colors.blue,
+                      //color: Colors.blue,
                       shape: new RoundedRectangleBorder(
                           borderRadius: new BorderRadius.circular(30.0))),
                 ),
                 Expanded(
-                    child: RaisedButton.icon(
+                    child: FlatButton.icon(
                         onPressed: () {
                           setState(() {
                             selectedButton = 1;
+                            _alignment = Alignment.bottomCenter;
                             print("selected Button Actual Location");
                           });
                         },
-                        icon: Icon(Icons.my_location),
+                        icon: Icon(Icons.my_location,
+                            color: selectedButton == 1
+                                ? Colors.white
+                                : Colors.grey),
                         label: Flexible(
                             child: Text("Actual Position",
-                                style: new TextStyle(fontSize: TEXT_SIZE),
+                                style: new TextStyle(
+                                    fontSize: TEXT_SIZE,
+                                    color: selectedButton == 1
+                                        ? Colors.white
+                                        : Colors.grey),
                                 textAlign: TextAlign.center)),
-                        color: Colors.red,
+                        //color: Colors.red,
                         shape: new RoundedRectangleBorder(
                             borderRadius: new BorderRadius.circular(30.0)))),
                 Expanded(
-                    child: RaisedButton.icon(
+                    child: FlatButton.icon(
                         onPressed: () {
                           setState(() {
                             selectedButton = 2;
+                            _alignment = Alignment.bottomRight;
                             print("selected Button Setting");
                           });
                         },
-                        icon: Icon(Icons.settings),
+                        icon: Icon(Icons.settings,
+                            color: selectedButton == 2
+                                ? Colors.white
+                                : Colors.grey),
                         label: Flexible(
                             child: Text("Settings",
-                                style: new TextStyle(fontSize: TEXT_SIZE),
+                                style: new TextStyle(
+                                    fontSize: TEXT_SIZE,
+                                    color: selectedButton == 2
+                                        ? Colors.white
+                                        : Colors.grey),
                                 textAlign: TextAlign.center)),
-                        color: Colors.green,
+                        //color: Colors.green,
                         shape: new RoundedRectangleBorder(
                             borderRadius: new BorderRadius.circular(30.0))))
-              ],
-            ))
-        );
+              ])
+            ])));
   }
 }
